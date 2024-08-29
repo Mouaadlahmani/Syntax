f1d1a4026cbb2da4ca85903630a4d98871685019
Mouaadlahmani <mouaadlahmani@gmail.com>
2024-08-29 17:14:55 +0100

    Creation de CoursServiceImpl

---
+++ Syntax/src/main/java/com/mouad/Syntax/controller/QuestionController.java
@@ -0,0 +1,41 @@
+package com.mouad.Syntax.controller;
+
+import com.mouad.Syntax.dto.QuestionDto;
+import com.mouad.Syntax.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/question/")
+public class QuestionController {
+    @Autowired
+    QuestionService questionService;
+
+    @PostMapping("create")
+    QuestionDto create(@RequestBody QuestionDto questionDto){
+        return questionService.addQuestion(questionDto);
+    }
+
+    @GetMapping("all")
+    List<QuestionDto> reponseAll(){
+        return questionService.getAll();
+    }
+
+    @GetMapping("{id}")
+    Optional<QuestionDto> reponseById(@PathVariable Long id){
+        return questionService.questionById(id);
+    }
+
+    @PutMapping("edit/{id}")
+    QuestionDto editReponse(@PathVariable Long id, @RequestBody QuestionDto questionDto){
+        return questionService.editQuestion(id, questionDto);
+    }
+
+    @DeleteMapping("delete/{id}")
+    void delete(@PathVariable Long id){
+        questionService.delete(id);
+    }
+}

c7106eaed517b3666b0cafb35144815112b11c21
Mouaadlahmani <mouaadlahmani@gmail.com>
2024-08-30 09:14:40 +0100

    Ajouter les methodes findAllByUtilisateur et findAllByCourses dans CertificatRepository

--- Syntax/src/main/java/com/mouad/Syntax/repository/CertificatRepository.java
+++ Syntax/src/main/java/com/mouad/Syntax/repository/CertificatRepository.java
@@ -1,9 +1,15 @@
 package com.mouad.Syntax.repository;
 
 import com.mouad.Syntax.model.Certificat;
+import com.mouad.Syntax.model.Cours;
+import com.mouad.Syntax.model.Utilisateur;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CertificatRepository extends JpaRepository<Certificat, Long> {
+    List<Certificat> findAllByUtilisateur(Utilisateur utilisateur);
+    List<Certificat> findAllByCourses(Cours cours);
 }

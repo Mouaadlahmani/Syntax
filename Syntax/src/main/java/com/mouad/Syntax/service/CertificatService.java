3f68c5a97dfa35c8b9ec17743c256d9bf17d2603
Mouaadlahmani <mouaadlahmani@gmail.com>
2024-08-30 09:15:12 +0100

    Creation de CertificatService

---
+++ Syntax/src/main/java/com/mouad/Syntax/service/CertificatService.java
@@ -0,0 +1,14 @@
+package com.mouad.Syntax.service;
+
+import com.mouad.Syntax.dto.CertificatDto;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CertificatService {
+    CertificatDto generateCertificat(Long id, CertificatDto certificatDto);
+    Optional<CertificatDto> getCertificatById(Long id);
+    List<CertificatDto> getCertificatList();
+    List<CertificatDto> getUtilisateurCertificatList(Long id);
+    List<CertificatDto> getCoursCertificats(Long id);
+}

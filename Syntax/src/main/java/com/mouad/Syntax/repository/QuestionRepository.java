2c728d6ec6fb60099e8d04595280771de30d30a2
Mouaadlahmani <mouaadlahmani@gmail.com>
2024-09-01 18:16:48 +0100

    Ajoouter les methods findByCours et findRandomQuestionByCours

--- Syntax/src/main/java/com/mouad/Syntax/repository/QuestionRepository.java
+++ Syntax/src/main/java/com/mouad/Syntax/repository/QuestionRepository.java
@@ -1,9 +1,22 @@
 package com.mouad.Syntax.repository;
 
+import com.mouad.Syntax.model.Cours;
 import com.mouad.Syntax.model.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface QuestionRepository extends JpaRepository<Question, Long> {
+    List<Question> findByCours(Cours cours);
+
+    @Query(value = "SELECT q.* FROM question q\n" +
+            "INNER JOIN cours c ON q.cours = c.id\n" +
+            "WHERE c.titre = :category\n" +
+            "ORDER BY RANDOM()\n" +
+            "LIMIT :numQ", nativeQuery = true)
+    List<Question> findRandomQuestionByCours(String category, int numQ);
+
 }

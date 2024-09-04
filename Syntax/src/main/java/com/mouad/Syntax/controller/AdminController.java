52eac877595d6ae30afed8eb38d4924acbf93ef3
Mouaadlahmani <mouaadlahmani@gmail.com>
2024-09-04 16:41:43 +0100

    Creation de AdminController

---
+++ Syntax/src/main/java/com/mouad/Syntax/controller/AdminController.java
@@ -0,0 +1,28 @@
+package com.mouad.Syntax.controller;
+
+import com.mouad.Syntax.model.Admin;
+import com.mouad.Syntax.service.AdminService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/admin/")
+public class AdminController {
+
+    @Autowired
+    AdminService service;
+
+    @GetMapping("all")
+    public List<Admin> getAll() {
+        return service.getAdmins();
+    }
+
+    @GetMapping("{id}")
+    public Optional<Admin> getAdminById(@PathVariable Long id) {
+        return service.getAdmin(id);
+    }
+
+}

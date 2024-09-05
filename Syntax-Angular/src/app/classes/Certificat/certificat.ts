bddd5ced347190a6b07a06c07b759cf3c5ad0817
Mouaadlahmani <mouaadlahmani@gmail.com>
2024-09-05 09:55:01 +0100

    Creation de Class Certificat

---
+++ Syntax-Angular/src/app/classes/Certificat/certificat.ts
@@ -0,0 +1,9 @@
+import {Cours} from "../Cours/cours";
+import {Utilisateur} from "../Utilisateur/utilisateur";
+
+export class Certificat {
+  id!: number;
+  dateObtention!: Date;
+  utilisateur!: Utilisateur;
+  courses!: Cours;
+}

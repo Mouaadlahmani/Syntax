01b965ed5285dd286088664933dd563ffac96a8c
Mouaadlahmani <mouaadlahmani@gmail.com>
2024-09-05 09:55:54 +0100

    Creation de Class Lecon

---
+++ Syntax-Angular/src/app/classes/Lecon/lecon.spec.ts
@@ -0,0 +1,7 @@
+import { Lecon } from './lecon';
+
+describe('Lecon', () => {
+  it('should create an instance', () => {
+    expect(new Lecon()).toBeTruthy();
+  });
+});
---
+++ Syntax-Angular/src/app/classes/Lecon/lecon.ts
@@ -0,0 +1,8 @@
+import {Cours} from "../Cours/cours";
+
+export class Lecon {
+  id!: number;
+  titre!: string;
+  contenu!: string;
+  courses!: Cours;
+}

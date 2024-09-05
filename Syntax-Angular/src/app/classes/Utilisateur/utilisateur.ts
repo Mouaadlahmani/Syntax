3aaab13d62af97745ab5df20e0f6dc7a74bb0c5d
Mouaadlahmani <mouaadlahmani@gmail.com>
2024-09-05 09:56:43 +0100

    Creation de Class Utilisateur

---
+++ Syntax-Angular/src/app/classes/Utilisateur/utilisateur.ts
@@ -0,0 +1,12 @@
+import {Certificat} from "../Certificat/certificat";
+import {Role} from "../enums/Role/role";
+
+export class Utilisateur {
+  id!: number;
+  nom!: number;
+  prenom!: number;
+  email!: number;
+  password!: number;
+  role!: Role;
+  certificats!: Certificat[];
+}

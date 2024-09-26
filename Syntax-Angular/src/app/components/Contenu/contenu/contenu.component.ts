097297cab1f753d0f599bf7d2ea8ab2b4b1a53f6
Mouaadlahmani <mouaadlahmani@gmail.com>
2024-09-26 11:10:41 +0100

    Creation de contenu

---
+++ Syntax-Angular/src/app/components/Contenu/contenu/contenu.component.ts
@@ -0,0 +1,11 @@
+import {Component, OnInit} from '@angular/core';
+import {Contenu} from "../../../classes/Contenu/contenu";
+import {LeconService} from "../../../services/lecon/lecon.service";
+import {ActivatedRoute, Router} from "@angular/router";
+
+@Component({
+  selector: 'app-contenu',
+  templateUrl: './contenu.component.html',
+  styleUrls: ['./contenu.component.css']
+})
+export class ContenuComponent {}

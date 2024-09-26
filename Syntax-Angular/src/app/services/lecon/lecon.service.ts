bae4666cd8a2822a8139b3a73766374aca1c1ce0
Mouaadlahmani <mouaadlahmani@gmail.com>
2024-09-26 10:56:58 +0100

    update lecon service

---
+++ Syntax-Angular/src/app/services/lecon/lecon.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Lecon} from "../../classes/Lecon/lecon";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LeconService {
+
+  private url = 'http://localhost:8081/api/lecon/';
+
+  constructor(private httpClient: HttpClient) { }
+
+  addLecon(id:number,lecon: Lecon):Observable<Object>{
+    return this.httpClient.post(`${this.url}add/${id}`, lecon);
+  }
+
+  modifyLecon(id:number, lecon: Lecon):Observable<Object>{
+    return this.httpClient.put(`${this.url}edit/${id}`, lecon)
+  }
+
+  deleteLecon(id:number): Observable<Lecon>{
+    return this.httpClient.delete<Lecon>(`${this.url}delete/${id}`)
+  }
+
+  getLeconById(id:number):Observable<Lecon>{
+    return this.httpClient.get<Lecon>(`${this.url}${id}`)
+  }
+}

1bf5a8a6ff8055bfa0e5db1e532c774ddc72773b
Mouaadlahmani <mouaadlahmani@gmail.com>
2024-09-26 10:49:35 +0100

    Update cours details

--- Syntax-Angular/src/app/components/Cours/cours-details/cours-details.component.ts
+++ Syntax-Angular/src/app/components/Cours/cours-details/cours-details.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { CoursService } from "../../../services/cours/cours.service";
-import { ActivatedRoute } from "@angular/router";
+import {ActivatedRoute, Route, Router} from "@angular/router";
 import { Cours } from "../../../classes/Cours/cours";
 import { Lecon } from "../../../classes/Lecon/lecon";
+import {LeconService} from "../../../services/lecon/lecon.service";
 
 @Component({
   selector: 'app-cours-details',
@@ -13,25 +14,47 @@ export class CoursDetailsComponent implements OnInit {
 
   id!: number;
   cours: Cours = new Cours();
-  selectedLecon: Lecon | null = null;
+  Lecons: Lecon[]=[];
 
-  constructor(private service: CoursService, private route: ActivatedRoute) { }
+  constructor(private service: CoursService,
+              private leconService:LeconService,
+              private route: ActivatedRoute,
+              private router:Router
+
+  ) { }
 
   ngOnInit(): void {
     this.id = this.route.snapshot.params['id'];
     this.service.getCoursById(this.id).subscribe(
       data => {
-        this.cours = data;
-        // Ensure that selectedLecon is reset when a new course is loaded
-        if (this.cours.lecons.length > 0) {
-          this.selectedLecon = this.cours.lecons[0];
-        }
+      this.Lecons = data.lecons;
       },
       error => console.error('Error fetching course', error)
     );
   }
-
-  selectLecon(lecon: Lecon): void {
-    this.selectedLecon = lecon;
+  courses(){
+    this.router.navigate(['courses'])
   }
+
+  modifyLecon(id: number | undefined){
+    this.router.navigate(['lecons/modify',id])
+  }
+  deleteLecon(id: number | undefined) {
+    if (id) {
+      this.leconService.deleteLecon(id).subscribe(
+        () => {
+          this.router.navigate(['courses/cours', this.id])
+        },
+        error => console.error('Error deleting leçon', error)
+      );
+    }
+  }
+  addLecon(id: number){
+    this.router.navigate(['lecons/add',this.id])
+  }
+
+  LeconDetails(id:number){
+    this.router.navigate(['contenu', id])
+  }
+
 }

cd5c2096aef27e7b89aaf982d2ffd7dfbf5dda06
Mouaadlahmani <mouaadlahmani@gmail.com>
2024-09-26 10:57:21 +0100

    update lecon details

---
+++ Syntax-Angular/src/app/components/Lecons/lecon-details/lecon-details.component.ts
@@ -0,0 +1,54 @@
+import {Component, OnInit} from '@angular/core';
+import {LeconService} from "../../../services/lecon/lecon.service";
+import {ActivatedRoute, Router} from "@angular/router";
+import {Contenu} from "../../../classes/Contenu/contenu";
+import {Lecon} from "../../../classes/Lecon/lecon";
+import {ContenuService} from "../../../services/contenu/contenu.service";
+
+@Component({
+  selector: 'app-lecon-details',
+  templateUrl: './lecon-details.component.html',
+  styleUrls: ['./lecon-details.component.css']
+})
+export class LeconDetailsComponent implements OnInit{
+
+  id!:number;
+  lecon!:Lecon;
+  content?:Contenu[]=[];
+
+  constructor(private service:LeconService,
+              private contenuService:ContenuService,
+              private route:ActivatedRoute,
+              private router:Router) {
+  }
+
+  ngOnInit(): void {
+    this.id = this.route.snapshot.params['id'];
+    this.getContenu();
+  }
+
+  getContenu(){
+    this.service.getLeconById(this.id).subscribe(
+      data=>{
+        this.lecon = data;
+        this.content = data.contenu;
+      }
+    )
+  }
+
+  addContenu(){
+    this.router.navigate(['contenu/add', this.id])
+  }
+  modifyContenu(id:number){
+  this.router.navigate(['contenu/modify', id])
+  }
+
+  deleteContenu(id:number){
+    this.contenuService.deleteContenu(id).subscribe(
+      data=>{
+       this.getContenu();
+      }
+    );
+  }
+
+}

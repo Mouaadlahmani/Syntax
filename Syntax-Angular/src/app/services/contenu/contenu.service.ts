54372f5a807aaf3bd671645bfa7b636cd21effc7
Mouaadlahmani <mouaadlahmani@gmail.com>
2024-09-26 11:11:18 +0100

    Creation de contenu service

---
+++ Syntax-Angular/src/app/services/contenu/contenu.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Contenu} from "../../classes/Contenu/contenu";
+import {Observable} from "rxjs";
+import {Cours} from "../../classes/Cours/cours";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ContenuService {
+
+  private url = 'http://localhost:8081/api/contenu/';
+
+  constructor(private httpClient: HttpClient) { }
+
+  addContenu(id:number, contenu: Contenu):Observable<Object>{
+    return this.httpClient.post(`${this.url}add/${id}`, contenu);
+  }
+  getCotenuById(id:number):Observable<Contenu>{
+    return this.httpClient.get<Contenu>(`${this.url}${id}`)
+  }
+
+  modifyContenu(id:number, contenu: Contenu):Observable<Object>{
+    return this.httpClient.put(`${this.url}edit/${id}`, contenu);
+  }
+
+  deleteContenu(id:number):Observable<Contenu>{
+    return this.httpClient.delete<Contenu>(`${this.url}delete/${id}`)
+  }
+}

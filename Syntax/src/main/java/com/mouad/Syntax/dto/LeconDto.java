c6ef0c1ae593ea117c6f0760578b16855ea30fd4
Mouaadlahmani <mouaadlahmani@gmail.com>
2024-09-26 10:00:37 +0100

    Update LeconDto

--- Syntax/src/main/java/com/mouad/Syntax/dto/LeconDto.java
+++ Syntax/src/main/java/com/mouad/Syntax/dto/LeconDto.java
@@ -1,12 +1,15 @@
 package com.mouad.Syntax.dto;
 
+import com.mouad.Syntax.model.Contenu;
 import com.mouad.Syntax.model.Cours;
 import lombok.Data;
+
+import java.util.List;
 
 @Data
 public class LeconDto {
     private Long id;
     private String titre;
-    private String contenu;
+    private List<Contenu> contenu;
     private Cours courses;
 }

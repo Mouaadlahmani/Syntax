1c5dc3fe288c56a1779d62d17b8f7c7adf3ac4cc
Mouaadlahmani <mouaadlahmani@gmail.com>
2024-09-26 11:01:10 +0100

    creation de dashbord component

---
+++ Syntax-Angular/src/app/components/Dashbord/dashbord/dashbord.component.ts
@@ -0,0 +1,11 @@
+import {Component, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
+import {Utilisateur} from "../../../classes/Utilisateur/utilisateur";
+
+@Component({
+  selector: 'app-dashbord',
+  templateUrl: './dashbord.component.html',
+  styleUrls: ['./dashbord.component.css']
+})
+export class DashbordComponent {
+}
